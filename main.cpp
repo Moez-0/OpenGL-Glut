e2a41b1611e6454cbeff4b213b14e18d5bf7fba2
Moez Souidi <moezlegend@gmail.com>
2023-01-07 20:42:41 +0100

    Added maincpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @author      : Moez (business@moez.codes)
+ * @file        : main
+ * @created     : Saturday Jan 07, 2023 20:41:49 WCAST
+ */
+
+#include <iostream>
+#include <GLUT/GLUT.h>
+int main()
+{
+    
+    return 0;
+}
+
